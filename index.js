a27ca2aae68afe628fd1303de81f249959b0ea10
RomanKonopelko <68011362+RomanKonopelko@users.noreply.github.com>
2020-10-18 11:21:20 +0300

    js-practice

---
+++ index.js
@@ -0,0 +1,42 @@
+// // task 1
+// let admin;
+// let name = "Джон";
+// admin = name;
+// alert(admin);
+
+// task 2
+// const leapYear = "Высокосный год";
+// const notLeapYear = "Невысокосный год";
+// const invalid = "invalid number";
+// let userYear = prompt();
+
+// console.log(userYear);
+
+// if (userYear === null) {
+//   alert("Отмена пользователем");
+// } else if (userYear == 0 || isNaN(userYear)) {
+//   alert(invalid);
+// } else if (userYear % 4 === 0 && userYear != 0) {
+//   alert(leapYear);
+// } else {
+//   alert(notLeapYear);
+// }
+
+// task 3
+// let userNumber = prompt("Введи число");
+// let compNumber = Math.floor(Math.random() * (11 - 1) + 1);
+// if (userNumber === null) {
+//   alert("Denied by user");
+// } else {
+//   if (userNumber === compNumber) {
+//     alert("Совпало", userNumber, compNumber);
+//   } else if (userNumber === NaN || (userNumber < 1 && userNumber > 10)) {
+//     alert("Введено невалидное значение");
+//   } else if (userNumber < 1 && userNumber > 10) {
+//     alert("Введено невалидное значение");
+//   } else {
+//     console.log("Не совпало", userNumber, compNumber);
+//   }
+// }
+
+
